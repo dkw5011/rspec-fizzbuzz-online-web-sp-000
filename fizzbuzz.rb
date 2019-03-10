fb5f8e78140af39e70bdc837efffe47be164fcef
Danielle Bergbower <dkw5011@gmail.com>
2019-03-10 14:37:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ if int % 3 == 0
   fizz_3 = "Fizz"
 elsif int % 5 == 0
 fizz_5 = "Buzz"
-elsif (int % 3 && int % 5) == 0
+elsif int % 3 == 0 && int % 5 == 0
 fizz_15 = "FizzBuzz"
 elsif int % 4 == 0
 fizz_4 = nil
