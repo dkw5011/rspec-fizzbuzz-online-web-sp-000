05cb95786a224037ad53beaabdf01e445a927c65
Danielle Bergbower <dkw5011@gmail.com>
2019-03-10 14:43:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-if int % 3 == 0
-  fizz_3 = "Fizz"
+if int % 3 == 0 && int % 5 == 0
+  fizz_3 = "FizzBuzz"
 elsif int % 5 == 0
 fizz_5 = "Buzz"
-elsif int % 3 == 0 && int % 5 == 0
-fizz_15 = "FizzBuzz"
-elsif int % 4 == 0
-fizz_4 = nil
+elsif int % 3 == 0 
+fizz_15 = "Fizz"
 end
 end
